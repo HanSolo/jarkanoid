c0f6526cb78c02568435d91b194c5f44cccd03a8
Gerrit Grunwald <han.solo@mac.com>
2023-02-19 09:12:23 +0100

    17.0.7

--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 set JAVA_VERSION=17
-set MAIN_JAR=jarkanoid-17.0.5.jar
-set APP_VERSION=17.0.5
+set MAIN_JAR=jarkanoid-17.0.7.jar
+set APP_VERSION=17.0.7
 
 rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
 rem Remove previously generated java runtime and installers. Copy all required
